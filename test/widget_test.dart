defab0a74a0384bb4903e091cb3fae6b2a11487a
Sivamuthu Kumar <ksivamuthu@ceiamerica.com>
2019-03-20 23:21:06 -0400

    Correct flutter test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,15 +4,4 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:dev_emotion_tracker/main.dart';
-
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-  });
-}
+void main() {}
